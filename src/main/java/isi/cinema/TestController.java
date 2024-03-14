96584d5cc25f429bf30abc77f8f99c0106d28f30
Pawel Kozera <pawelkozera1@gmail.com>
2024-03-14 12:16:14 +0100

    Added TestController to test api

---
+++ src/main/java/isi/cinema/TestController.java
@@ -0,0 +1,26 @@
+package isi.cinema;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+    @CrossOrigin
+    @GetMapping("/test")
+    public String getUsers() throws JsonProcessingException {
+        List<String> userList = new ArrayList<>();
+
+        userList.add("Test");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(userList);
+    }
+}
